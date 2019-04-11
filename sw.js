02959e6fa74d1de92d9cfa2e70ebb4607101e654
Konstantinos Katsoulas <katsoulk@gmail.com>
2019-04-12 02:07:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,63 @@
 const CACHE_NAME = "V1"
+const expectedCaches = [CACHE_NAME];
+const staticFiles = [
+		'/',
+		'/index.html',
+        '/main.css',
+        '/main.js',
+];
+
+
+
+/**
+ * Performs install steps.
+ */
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(staticFiles))
+  );
+});
+
+/**
+ * Handles requests: responds with cache or else network.
+ */
+self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+/**
+ * Cleans up static cache and activates the Service Worker.
+ */
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map((key) => {
+        if (!expectedCaches.includes(key)) {
+          return caches.delete(key);
+        }
+      })
+    )).then(() => {
+      console.log(`${CACHE_NAME} now ready to handle fetches!`);
+      return clients.claim();
+    }).then(() => {
+      return self.clients.matchAll().then(clients =>
+        Promise.all(clients.map(client =>
+          client.postMessage('The service worker has activated and taken control. This application can now be used offline.')
+        ))
+      );
+    })
+  );
+});
 
 /**
  * The install event is fired when the registration succeeds. 
  * After the install step, the browser tries to activate the service worker.
  * Generally, we cache static resources that allow the website to run offline
- */
+
 this.addEventListener('install', async function() {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
@@ -30,63 +83,10 @@ self.addEventListener('activate', function(e) {
     return self.clients.claim();
 });
   
-/**
- * The fetch event is fired every time the browser sends a request. 
- * In this case, the service worker acts as a proxy. We can for example return the cached
- * version of the ressource matching the request, or send the request to the internet
- * , we can even make our own response from scratch !
- * Here, we are going to use cache first strategy
- */
- /*
-self.addEventListener('fetch', event => {
-    //We defind the promise (the async code block) that return either the cached response or the network one
-    //It should return a response object
-    const getCustomResponsePromise = async => {
-        console.log(`URL ${event.request.url}`, `location origin ${location}`)
-
-        try {
-            //Try to get the cached response
-            const cachedResponse = await caches.match(event.request)
-            if (cachedResponse) {
-                //Return the cached response if present
-                console.log(`Cached response ${cachedResponse}`)
-                return cachedResponse
-            }
-
-            //Get the network response if no cached response is present
-            const netResponse = await fetch(event.request)
-            console.log(`adding net response to cache`)
-
-            //Here, we add the network response to the cache
-            let cache = await caches.open(CACHE_NAME)
-
-            //We must provide a clone of the response here
-            cache.put(event.request, netResponse.clone())
-
-            //return the network response
-            return netResponse
-        } catch (err) {
-            console.error(`Error ${err}`)
-            throw err
-        }
-    }
-
-    //In order to override the default fetch behavior, we must provide the result of our custom behavoir to the
-    //event.respondWith method
-    event.respondWith(getCustomResponsePromise())
-})*/
-
 
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
-  var dataUrl = 'https://query.yahooapis.com/v1/public/yql';
-    /*
-     * When the request URL contains dataUrl, the app is asking for fresh
-     * weather data. In this case, the service worker always goes to the
-     * network and then caches the response. This is called the "Cache then
-     * network" strategy:
-     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
-     */
+
     e.respondWith(
       caches.open(CACHE_NAME).then(function(cache) {
         return fetch(e.request).then(function(response){
@@ -97,3 +97,4 @@ self.addEventListener('fetch', function(e) {
     );
 
 });
+ */
