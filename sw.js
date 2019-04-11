d4afe2e27d3503d56e4ff63f445da02dd6dedfca
Konstantinos Katsoulas <katsoulk@gmail.com>
2019-04-12 02:22:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,54 +10,10 @@ const staticFiles = [
 
 
 /**
- * Performs install steps.
- */
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(staticFiles))
-  );
-});
-
-/**
- * Handles requests: responds with cache or else network.
- */
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
-});
-
-/**
- * Cleans up static cache and activates the Service Worker.
- */
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.map((key) => {
-        if (!expectedCaches.includes(key)) {
-          return caches.delete(key);
-        }
-      })
-    )).then(() => {
-      console.log(`${CACHE_NAME} now ready to handle fetches!`);
-      return clients.claim();
-    }).then(() => {
-      return self.clients.matchAll().then(clients =>
-        Promise.all(clients.map(client =>
-          client.postMessage('The service worker has activated and taken control. This application can now be used offline.')
-        ))
-      );
-    })
-  );
-});
-
-/**
  * The install event is fired when the registration succeeds. 
  * After the install step, the browser tries to activate the service worker.
  * Generally, we cache static resources that allow the website to run offline
-
+*/
 this.addEventListener('install', async function() {
     const cache = await caches.open(CACHE_NAME);
     cache.addAll([
@@ -97,4 +53,4 @@ self.addEventListener('fetch', function(e) {
     );
 
 });
- */
+ 
