ee96fbadbb252a763226028d792e5a541bd04a31
Konstantinos Katsoulas <katsoulk@gmail.com>
2019-04-12 02:35:44 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -39,7 +39,44 @@ self.addEventListener('activate', function(e) {
     return self.clients.claim();
 });
   
+self.addEventListener('fetch', event => {
+    //We defind the promise (the async code block) that return either the cached response or the network one
+    //It should return a response object
+    const getCustomResponsePromise = async => {
+        console.log(`URL ${event.request.url}`, `location origin ${location}`)
 
+        try {
+            //Try to get the cached response
+            const cachedResponse = await caches.match(event.request)
+            if (cachedResponse) {
+                //Return the cached response if present
+                console.log(`Cached response ${cachedResponse}`)
+                return cachedResponse
+            }
+
+            //Get the network response if no cached response is present
+            const netResponse = await fetch(event.request)
+            console.log(`adding net response to cache`)
+
+            //Here, we add the network response to the cache
+            let cache = await caches.open(CACHE_NAME)
+
+            //We must provide a clone of the response here
+            cache.put(event.request, netResponse.clone())
+
+            //return the network response
+            return netResponse
+        } catch (err) {
+            console.error(`Error ${err}`)
+            throw err
+        }
+    }
+
+    //In order to override the default fetch behavior, we must provide the result of our custom behavoir to the
+    //event.respondWith method
+    event.respondWith(getCustomResponsePromise())
+})
+/*
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
 
@@ -52,5 +89,6 @@ self.addEventListener('fetch', function(e) {
       })
     );
 
-});
+});*/
+
  
