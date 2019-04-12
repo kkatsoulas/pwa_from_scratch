21fa66bd1f9084a5ef761efceaafcbf0dd162359
Konstantinos Katsoulas <katsoulk@gmail.com>
2019-04-12 23:27:48 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -38,7 +38,45 @@ self.addEventListener('activate', function(e) {
   );
     return self.clients.claim();
 });
+
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://api.jikan.moe/';
   
+  const cachedResponse
+  
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
+
+/*  
 self.addEventListener('fetch', event => {
     //We defind the promise (the async code block) that return either the cached response or the network one
     //It should return a response object
@@ -75,7 +113,7 @@ self.addEventListener('fetch', event => {
     //In order to override the default fetch behavior, we must provide the result of our custom behavoir to the
     //event.respondWith method
     event.respondWith(getCustomResponsePromise())
-})
+})*/
 /*
 self.addEventListener('fetch', function(e) {
   console.log('[Service Worker] Fetch', e.request.url);
